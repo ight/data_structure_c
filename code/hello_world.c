62743d034cb5e5986141fd280fbcdcb8c404b615
prakash bhakat <prakash.bhakat2@tcs.com>
2017-12-08 17:08:48 +0530

    DataStructure: learned hello world

---
+++ code/hello_world.c
@@ -0,0 +1,11 @@
+/* simple hello world program */
+# include <stdio.h>
+
+int main() 
+{
+	/* printf (“format string”, argument list); this is the syntax of printf function 
+	where formated string under "" this will be printed as it is, it also acept argument
+	which we will see in the next hello world program*/ 
+	printf("Hello World Back To C, to bring back the programmer\n");
+	return 0;
+}
