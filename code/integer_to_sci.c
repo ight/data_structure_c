05e9696c4d0dd1fcd472ae2eb2ea0741379170df
ight <thelight.prakash247@gmail.com>
2018-01-02 21:31:45 +0530

    DataStructure: Practice of ASCII

---
+++ code/integer_to_sci.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	int chr;
+	printf("Enter a number and we will try to figure out it's charater code");
+	scanf("%d", &chr);
+	printf("You entered %d and it's charater code is: %c \n", chr, chr);
+	return 0;
+}
