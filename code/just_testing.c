af280b72d81723a2a7a6ad2c80fc63cea320767a
thelight.prakash247@gmail.com <ight>
2018-01-12 08:51:42 +0530

    DataStructure: Testing
     * Setup seprate username and email for github

---
+++ code/just_testing.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+	printf("I should be free from git setup \n");
+	return 0;
+}
---
+++ use_github.sh
@@ -0,0 +1,2 @@
+git config user.name $GIT_HUB_EMAIL
+git config user.email $GIT_HUB_USER_NAME
