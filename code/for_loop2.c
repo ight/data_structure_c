61af0722db133fe5cc713fbdd334407b15520e1f
thelight.prakash247@gmail.com <ight>
2018-01-23 23:12:21 +0530

    DataStructure: For loop

---
+++ code/for_loop2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  int counter; //just declare the counter
+  int start = 0; //declare and initilize the initial value
+  int end = 0; // declare and initilize the end value.
+  int sum = 0; //declare and imtialize sum
+  printf("We will find the sum for number from 0..10\n");
+  for(counter = -10; counter <= end; counter++)
+  {
+    sum = sum + counter;
+  }
+  printf("The sum of numbers from 0..10: %d \n", sum);
+  return 0;
+}
